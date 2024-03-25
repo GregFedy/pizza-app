51c36380664f861803ec548f52ad2f97e20c4f34
Grigorii <mr.fursy@gmail.com>
2024-03-25 21:02:19 +0300

    feat: add Input component

---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,17 @@
+import { forwardRef } from 'react';
+
+type InputProps = React.ComponentPropsWithRef<'input'>;
+
+type Ref = HTMLInputElement;
+
+const Input = forwardRef<Ref, InputProps>((props, ref) => {
+	return (
+		<input
+			ref={ref}
+			className="h-[65px] w-[324px] rounded-[10px] border p-5 font-normal text-[17px] placeholder:text-gray-light"
+			{...props}
+		/>
+	);
+});
+
+export default Input;
