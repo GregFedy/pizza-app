74d6affaf3b27ea23a2c2e4ef48f5599c7605edc
Grigorii <mr.fursy@gmail.com>
2024-03-30 16:57:04 +0300

    feat: add base Layout and hollow Menu, Cart as placeholders

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Cart from './pages/Cart/Cart';
+import Layout from './layout/Layout';
+import Menu from './pages/Menu/Menu';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Layout />,
+		children: [
+			{
+				path: '/',
+				element: <Menu />,
+			},
+			{
+				path: '/cart',
+				element: <Cart />,
+			},
+		],
+	},
+]);
+
 const App = () => {
 	return (
 		<div>
-			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			<RouterProvider router={router} />
 		</div>
 	);
 };
---
+++ src/layout/Layout.tsx
@@ -0,0 +1,39 @@
+import { NavLink, Outlet } from 'react-router-dom';
+import Button from '../components/ui/Button';
+
+const Layout = () => {
+	return (
+		<div className="flex">
+			<div className="flex min-h-screen flex-col border-[#D4D6E0] border-r p-8">
+				<div className="mb-12">
+					<img
+						className="mb-5 drop-shadow-[0_8px_40px_rgba(255,197,41,0.25)]"
+						src="/avatar.png"
+						alt="User avatar"
+					/>
+					<div className="font-bold text-xl">Григорий Федоренко</div>
+					<div className="text-[#9EA1B1] text-sm">gregory@gmail.com</div>
+				</div>
+				<div className="flex flex-col gap-8">
+					<NavLink className="flex items-center gap-4" to="/">
+						<img src="/menu-icon.svg" alt="Menu icon" />
+						Меню
+					</NavLink>
+					<NavLink className="flex items-center gap-4" to="/cart">
+						<img src="/cart-icon.svg" alt="Cart icon" />
+						Корзина
+					</NavLink>
+				</div>
+				<Button
+					size="small"
+					className="mt-auto flex h-[43px] w-[117px] items-center justify-center gap-[9px] rounded-[28px] bg-orange font-normal text-[15px] text-white leading-4 hover:bg-orange-dark"
+				>
+					<img src="/exit-icon.svg" alt="Exit button" />
+					<span className="pt-[13px] pr-[14px] pb-[14px]">Выйти</span>
+				</Button>
+			</div>
+			<Outlet />
+		</div>
+	);
+};
+export default Layout;
