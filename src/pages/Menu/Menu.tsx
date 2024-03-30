ac86dff399cad19c0efb88c77148f731cd9a16d1
Grigorii <mr.fursy@gmail.com>
2024-03-30 18:50:44 +0300

    feat: insert Heading & Search into Menu

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,4 +1,12 @@
+import Heading from '../../components/ui/Heading';
+import Search from '../../components/Search/Search';
+
 const Menu = () => {
-	return <div>Menu</div>;
+	return (
+		<div className="flex justify-between">
+			<Heading>Меню</Heading>
+			<Search placeholder="Введите блюдо или состав" />
+		</div>
+	);
 };
 export default Menu;
