14f5250f6d462a5bee14df8b929baf7d1670fe62
Grigorii <mr.fursy@gmail.com>
2024-03-31 17:31:36 +0300

    feat: insert ProductCard into Menu

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,12 +1,25 @@
 import Heading from '../../components/ui/Heading';
 import Search from '../../components/Search/Search';
+import ProductCard from '../../components/ProductCard/ProductCard';
 
 const Menu = () => {
 	return (
-		<div className="flex justify-between">
-			<Heading>Меню</Heading>
-			<Search placeholder="Введите блюдо или состав" />
-		</div>
+		<>
+			<div className="mb-10 flex justify-between">
+				<Heading>Меню</Heading>
+				<Search placeholder="Введите блюдо или состав" />
+			</div>
+			<div>
+				<ProductCard
+					id={1}
+					title="Наслаждение"
+					description="Салями, руккола, помидоры, оливки"
+					price={300}
+					image="/image-80.png"
+					rating={4.5}
+				/>
+			</div>
+		</>
 	);
 };
 export default Menu;
