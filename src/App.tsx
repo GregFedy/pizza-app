073735fa06b86cc7c0cec65203eb22f1c16e906d
Grigorii Fedorenko <mr.fursy@gmail.com>
2024-04-10 12:38:40 +0300

    feat: add loader, errorElement

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import Cart from './pages/Cart/Cart';
 import Layout from './layout/Layout';
 import Menu from './pages/Menu/Menu';
 import ProductInfo from './components/ProductInfo/ProductInfo';
+import axios from 'axios';
+import { API_PREFIX } from './helpers/API';
+import type { Product } from './types/product.type';
 
 const router = createBrowserRouter([
 	{
@@ -20,6 +23,13 @@ const router = createBrowserRouter([
 			{
 				path: '/product/:id',
 				element: <ProductInfo />,
+				errorElement: <h2 className="text-xl">Ошибка в загрузке данных</h2>,
+				loader: async ({ params }) => {
+					const { data } = await axios.get<Product>(
+						`${API_PREFIX}/products/${params.id}`,
+					);
+					return data;
+				},
 			},
 		],
 	},
