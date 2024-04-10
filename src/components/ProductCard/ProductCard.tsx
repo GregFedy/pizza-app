953d19ffae5912b9827ad9f999c0d046e5658a3a
Grigorii Fedorenko <mr.fursy@gmail.com>
2024-04-10 13:36:14 +0300

    styles: change background size rule

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -14,7 +14,7 @@ const ProductCard = (props: ProductCardProps) => {
 		<Link to={`/product/${props.id}`}>
 			<div className="w-80 border-white drop-shadow-[18px_18px_36px_rgba(211,209,216,0.25)]">
 				<div
-					className="relative mb-1 min-h-40 rounded-2xl"
+					className="relative mb-1 min-h-40 rounded-2xl bg-cover"
 					style={{
 						backgroundImage: `url(http://localhost:4000/pizza-api-v1/${props.image}`,
 					}}
