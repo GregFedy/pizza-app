9a2f2ad9422d821912307e01126058fa34991e70
Grigorii Fedorenko <mr.fursy@gmail.com>
2024-04-14 10:43:59 +0300

    styles: remove width style

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -8,7 +8,7 @@ const Input = forwardRef<Ref, InputProps>((props, ref) => {
 	return (
 		<input
 			ref={ref}
-			className="h-[65px] w-[324px] rounded-[10px] border p-5 font-normal text-[17px] placeholder:text-gray-light"
+			className="h-[65px] rounded-[10px] border p-5 font-normal text-[17px] placeholder:text-gray-light"
 			{...props}
 		/>
 	);
