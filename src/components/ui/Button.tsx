fe62ddfaf441f5b7ee190135f838f1e0ef784ee5
Grigorii Fedorenko <mr.fursy@gmail.com>
2024-04-14 10:47:34 +0300

    styles: add shadow

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,9 +4,9 @@ type ButtonProps = {
 
 const Button = ({ children, type, size, ...props }: ButtonProps) => {
 	const buttonStyles = {
-		big: 'h-[60px] w-[248px] rounded-[28px] bg-orange font-semibold text-[15px] text-white uppercase leading-5 hover:bg-orange-dark',
+		big: 'h-[60px] w-[248px] rounded-[28px] bg-orange font-semibold text-[15px] text-white uppercase leading-5 hover:bg-orange-dark shadow-[0_10px_40px_0_rgba(122,129,190,0.3)]',
 		small:
-			'h-[43px] w-[117px] rounded-[28px] bg-orange font-normal text-[15px] text-white leading-4 hover:bg-orange-dark',
+			'h-[43px] w-[117px] rounded-[28px] bg-orange font-normal text-[15px] text-white leading-4 hover:bg-orange-dark shadow-[0_10px_40px_0_rgba(122,129,190,0.3)]',
 	};
 	return (
 		<button type={type} className={buttonStyles[size]} {...props}>
