74e1c859182dc2060b152722b6497727a2f6baa6
Grigorii Fedorenko <mr.fursy@gmail.com>
2024-04-15 15:15:39 +0300

    feat: add usersReducer to store & subscribe jwt

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
+import usersReducer, { USER_DATA } from './userSlice';
+import { saveState } from '../helpers/storage';
 
 export const store = configureStore({
-	reducer: {},
+	reducer: {
+		users: usersReducer,
+	},
+});
+
+store.subscribe(() => {
+	saveState(
+		{
+			jwt: store.getState().users.jwt,
+		},
+		USER_DATA,
+	);
 });
 
 // Получаем типы `RootState` и `AppDispatch` из самого store
